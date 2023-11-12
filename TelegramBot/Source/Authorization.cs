a71f89a433ef4f31f65ca51d6b8d44d7dcf993d0
elnfach <vpetuhov160@gmail.com>
2023-11-12 19:28:13 +0800

    commit

--- TelegramBot/Source/Authorization.cs
+++ TelegramBot/Source/Authorization.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using TL;
 
 namespace TelegramBot.Source
 {
     internal static class Authorization
     {
-        public static void Auth()
+        private static WTelegram.Client _client;
+        public static async void Auth(string phone, string apiID, string apiHash)
         {
+            _client = new WTelegram.Client(int.Parse(apiID), apiHash);
+            await DoLogin(phone);
+        }
 
+        private static async Task DoLogin(string loginInfo)
+        {
+            string what = await _client.Login(loginInfo);
+            if (what != null)
+            {
+                RenderDesign.ShowButtonSendCode();
+                return;
+            }
         }
     }
 }
---
+++ TelegramBot/Source/RenderDesign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Source
+{
+    internal static class RenderDesign
+    {
+        public static void ShowButtonSendCode()
+        {
+
+        }
+    }
+}
