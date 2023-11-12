1caac97b0534a0ad156016bf7f47771b2500f272
elnfach <vpetuhov160@gmail.com>
2023-11-12 19:15:21 +0800

    include wtelegram

---
+++ TelegramBot/Source/Authorization.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Source
+{
+    internal static class Authorization
+    {
+        public static void Auth()
+        {
+
+        }
+    }
+}
